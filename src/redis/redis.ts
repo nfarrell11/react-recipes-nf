9b004152d7af2529c0f0349b1e07f9d1b4c03088
nfarrell11 <116067559+nfarrell11@users.noreply.github.com>
2025-12-12 19:34:34 -0800

    Updated redis token

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,6 +1,6 @@
 import { env } from "@/data/env/server"
 import { Redis } from "@upstash/redis"
-
+// Manages authentication and session tokens
 export const redisClient = new Redis({
   url: env.REDIS_URL,
   token: env.REDIS_TOKEN,
